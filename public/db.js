8432f55d5e54cb4debac7da5e7807319cdf3db6d
Tskading <Tskading@gmail.com>
2021-02-20 14:56:47 -0800

    Offline functionality added.

--- public/db.js
+++ public/db.js
@@ -1,6 +1,7 @@
-const { request } = require("express");
-
+// const { request } = require("express");
 let db;
+
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(e) {
   const db = e.target.result;
@@ -19,12 +20,40 @@ request.onerror = function(e) {
   console.log("Houston, we found an error: " + e.target.errorCode);
 };
 
-function saveRecord() {
-    
-};
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+
+  console.log(record);
+}
 
 function checkDb() {
+  const transaction = db.transaction(["pending"], "readwrite");
 
-};
+  const store = transaction.objectStore("pending");
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          transaction = db.transaction(["pending"], "readwrite");
+          store.clear();
+        });
+    }
+  };
+}
 
 window.addEventListener("online", checkDb);
